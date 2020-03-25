96eafe22890601192f1677edbde9b75651f39b6d
Mikhail Buslau <mikhailbuslau@gmail.com>
2020-03-25 12:38:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,3 +13,15 @@ function logShout(str) {
 function logWhisper(str) {
   console.log(str.toLowerCase());
 }
+
+function sayHiToGrandma(str) {
+  let uppercase = str;
+  let lowercase = str;
+  if (lowercase.toLowerCase() === lowercase) {
+    return `I can't hear you!`;
+  } else if (uppercase.toUpperCase() === uppercase) {
+    return `YES INDEED!`;
+  } else if (str === `I love you, Grandma.`) {
+    return `I love you, too.`;
+  }
+}
